3f1fb4197141c36fa6e4cbfc267ca03bd9ce9775
dustycake <dustycake1029@gmail.com>
2026-05-07 16:00:45 +0800

    your message

--- caps_project/frontend/src/App.js
+++ caps_project/frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Note from "./Note";
+import NotePage from "./NotePage"; 
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/note" element={<Note />} />
+        <Route path="/note/:id" element={<NotePage />} />
       </Routes>
     </Router>
   );
